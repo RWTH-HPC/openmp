fcc465f7a3589b74e402bc0048a6c515b6cccf4b
convent <simon.convent@rwth-aachen.de>
2017-01-12 11:23:15 +0100

    restructure test folder

---
+++ runtime/test/ompt/worksharing/sections.c
@@ -0,0 +1,26 @@
+// RUN: %libomp-compile-and-run | FileCheck %s
+// REQUIRES: ompt
+#include "callback.h"
+#include <omp.h>
+
+int main()
+{
+  #pragma omp parallel sections num_threads(2)
+  {
+    #pragma omp section
+    {
+      printf("%d: section 1\n", ompt_get_thread_data().value);
+    }
+    #pragma omp section
+    {
+      printf("%d: section 2\n", ompt_get_thread_data().value);
+    }
+  }
+
+  // Check if libomp supports the callbacks for this test.
+  // CHECK-NOT: {{^}}0: Could not register callback 'ompt_callback_work'
+
+  // CHECK: 0: NULL_POINTER=[[NULL:.*$]]
+
+  return 0;
+}
