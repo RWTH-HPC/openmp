c31d4fb2808722dc019217b6769ecef021ca3394
convent <simon.convent@rwth-aachen.de>
2017-02-23 13:09:02 +0100

    restructure ompt cancel testcases

--- runtime/test/ompt/cancel/cancel_parallel.c
+++ runtime/test/ompt/cancel/cancel_parallel.c
@@ -1,12 +1,21 @@
 // RUN: %libomp-compile && env OMP_CANCELLATION=true %libomp-run | %sort-threads | FileCheck %s
 // REQUIRES: ompt
 #include "callback.h"
+#include "omp.h"
 
 int main()
 {
-  #pragma omp parallel num_threads(1)
+  #pragma omp parallel num_threads(2)
   {
-    #pragma omp cancel parallel
+    if(omp_get_thread_num() == 0)
+    {
+      #pragma omp cancel parallel
+    }
+    else
+    {
+      sleep(1);
+      #pragma omp cancellation point parallel
+    }
   }
 
 
@@ -16,6 +25,7 @@ int main()
   // CHECK: {{^}}0: NULL_POINTER=[[NULL:.*$]]
   // CHECK: {{^}}[[MASTER_ID:[0-9]+]]: ompt_event_task_create: parent_task_id=[[PARENT_TASK_ID:[0-9]+]], parent_task_frame.exit=[[NULL]], parent_task_frame.reenter=[[NULL]], new_task_id=[[TASK_ID:[0-9]+]], parallel_function={{0x[0-f]*}}, task_type=ompt_task_initial=1, has_dependences=no
   // CHECK: {{^}}[[MASTER_ID]]: ompt_event_cancel: task_data=[[TASK_ID:[0-9]+]], flags=ompt_cancel_parallel|ompt_cancel_activated=17, codeptr_ra={{0x[0-f]*}}
+  // CHECK: {{^}}[[OTHER_THREAD_ID:[0-9]+]]: ompt_event_cancel: task_data=[[TASK_ID:[0-9]+]], flags=ompt_cancel_parallel|ompt_cancel_detected=33, codeptr_ra={{0x[0-f]*}}
 
   return 0;
 }
