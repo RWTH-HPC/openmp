615e12469e871832e69b25e303065a5cc8ea1571
convent <simon.convent@rwth-aachen.de>
2017-03-10 11:43:53 +0100

    add more tests to api_calls testcase

--- runtime/test/ompt/misc/api_calls.c
+++ runtime/test/ompt/misc/api_calls.c
@@ -1,7 +1,19 @@
-// RUN: %libomp-compile-and-run | FileCheck %s
+// RUN: %libomp-compile && env OMP_PLACES=cores %libomp-run | FileCheck %s
 // REQUIRES: ompt
 #include "callback.h"
 #include <omp.h>
+#include <sched.h>
+
+void print_list(char* function_name, int list[])
+{
+  printf("%" PRIu64 ": %s(0)=(%d", ompt_get_thread_data()->value, function_name, list[0]);
+  int i;
+  for(i = 1; i < omp_get_place_num_procs(0); i++)
+  {
+    printf(",%d", list[i]);
+  }
+  printf(")\n");
+}
 
 int main()
 {
@@ -10,9 +22,21 @@ int main()
   	printf("%" PRIu64 ": omp_get_num_places()=%d\n", ompt_get_thread_data()->value, omp_get_num_places());
   	printf("%" PRIu64 ": ompt_get_num_places()=%d\n", ompt_get_thread_data()->value, ompt_get_num_places());
 
+    //place_proc_ids
+    int omp_ids[omp_get_place_num_procs(0)];
+    omp_get_place_proc_ids(0, omp_ids);
+    print_list("omp_get_place_proc_ids" ,omp_ids);
+    int ompt_ids[omp_get_place_num_procs(0)];
+    ompt_get_place_proc_ids(0, omp_get_place_num_procs(0), ompt_ids);
+    print_list("ompt_get_place_proc_ids", ompt_ids);
+
   	printf("%" PRIu64 ": omp_get_place_num()=%d\n", ompt_get_thread_data()->value, omp_get_place_num());
   	printf("%" PRIu64 ": ompt_get_place_num()=%d\n", ompt_get_thread_data()->value, ompt_get_place_num());
 
+    //partition_place_nums
+
+    printf("%" PRIu64 ": sched_getcpu()=%d\n", ompt_get_thread_data()->value, sched_getcpu());
+    printf("%" PRIu64 ": ompt_get_proc_id()=%d\n", ompt_get_thread_data()->value, ompt_get_proc_id());
   }
 
   // Check if libomp supports the callbacks for this test.
@@ -21,8 +45,16 @@ int main()
 
   // CHECK: {{^}}[[MASTER_ID:[0-9]+]]: omp_get_num_places()=[[NUM_PLACES:[0-9]+]]
   // CHECK: {{^}}[[MASTER_ID]]: ompt_get_num_places()=[[NUM_PLACES]]
-  // CHECK: {{^}}[[MASTER_ID:[0-9]+]]: omp_get_place_num()=[[PLACE_NUM:[-]?[0-9]+]]
+
+  // CHECK: {{^}}[[MASTER_ID]]: omp_get_place_proc_ids(0)=([[PROC_IDS:[0-9\,]+]])
+  // CHECK: {{^}}[[MASTER_ID]]: ompt_get_place_proc_ids(0)=([[PROC_IDS]])
+
+  // CHECK: {{^}}[[MASTER_ID]]: omp_get_place_num()=[[PLACE_NUM:[-]?[0-9]+]]
   // CHECK: {{^}}[[MASTER_ID]]: ompt_get_place_num()=[[PLACE_NUM]]
+
+  // CHECK: {{^}}[[MASTER_ID]]: sched_getcpu()=[[CPU_ID:[0-9]+]]
+  // CHECK: {{^}}[[MASTER_ID]]: ompt_get_proc_id()=[[CPU_ID]]
+
 
   return 0;
 }
