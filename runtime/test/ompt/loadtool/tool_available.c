57fb543806982e8eb20aa75adae73fc612264c1b
convent <simon.convent@rwth-aachen.de>
2017-06-08 11:12:56 +0200

    add testcase for OMP_TOOL_LIBRARIES

---
+++ runtime/test/ompt/loadtool/tool_available.c
@@ -0,0 +1,20 @@
+// RUN: %libomp-tool %s.tool.c && %libomp-compile && env OMP_TOOL_LIBRARIES=%t.tool.so %libomp-run | %sort-threads | FileCheck %s
+// REQUIRES: ompt, cancel
+
+#include "omp.h"
+
+int main()
+{
+  #pragma omp parallel num_threads(2)
+  {
+  }
+
+
+  // Check if libomp supports the callbacks for this test.
+  // CHECK-NOT: {{^}}0: Could not register callback 
+  
+  // CHECK: {{^}}0: NULL_POINTER=[[NULL:.*$]]
+  // CHECK: {{^}}0: ompt_event_runtime_shutdown
+
+  return 0;
+}
